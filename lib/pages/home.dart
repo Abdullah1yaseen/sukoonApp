7cd4a88507ec368f772b68a31c31fe94ae1e7704
Abdullah Yaseen <a0bdullah.yaseen01@gmail.com>
2026-05-14 19:15:02 +0300

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {        
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+  title: Text("Sukoon",style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),),
+  centerTitle: true,
+backgroundColor: Color(0xFF1E3A5F),  
+),
+
+    );
+  }
+}
