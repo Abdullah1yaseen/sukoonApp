47920ecc1ade3ffe96246a6b135d98fdb38d799b
Abdullah Yaseen <a0bdullah.yaseen01@gmail.com>
2026-05-14 19:21:34 +0300

    add text box

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class HomePage extends StatelessWidget {        
   const HomePage({super.key});
 
@@ -11,7 +10,12 @@ appBar: AppBar(
   centerTitle: true,
 backgroundColor: Color(0xFF1E3A5F),  
 ),
-
+  body: Container(
+    width: double.infinity,
+    height: 200,
+    color: Colors.blue,
+    child: Text("طريقة عمل سُكون"),
+  )
     );
   }
 }
