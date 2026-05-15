97e7db20fa2068b576809fd24631c490669b4a97
Abdullah Yaseen <a0bdullah.yaseen01@gmail.com>
2026-05-15 20:28:26 +0300

    change fint color to white, and the body to dark blue

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -36,6 +36,7 @@ child: Column(
       "طريقة عمل سُكون",
       style: TextStyle(
         fontSize: 22,
+        color: Colors.white,
 
         ),
         ),
@@ -43,22 +44,26 @@ child: Column(
           "تأكد من تشغيل البلوتوث",
           style: TextStyle(
             fontSize: 16,
+            color: Colors.white,
           ),
           ),
     Text("تأكد من وجود جهاز سُكون قريب منك",
     style: TextStyle(
       fontSize: 16,
+      color: Colors.white,
     ),
     ),
     Text("منح الاذونات اللازمة للعمل بشكل صحيح",
     style: TextStyle(
       fontSize: 16,
+      color: Colors.white,
     ),)
   ],
 ),
 
   ),
  ), 
+ backgroundColor: Color(0xFF0F172A),
     );
   }
 }
